2312c43d7b3c45cb336f85b585e5005bb2386c7b
ram-manthry <ram.manthry@gmail.com>
2014-09-28 06:03:57 +1300

    added error handling pages

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
-var express = require('express');
+var express = require('express'),
+    exphbs  = require('express-handlebars'),
+    logger = require('morgan');
+
+// ### Middleware to handle 404
+var notFound = function(req,res,next){
+    res.statusCode = 404;
+    res.description = 'Not found';
+    res.render('404');
+};
+// ### Middleware to internal server errors
+var errorHandler = function(err,req,res,next){
+    res.render('error',{description:'Please check the URL.'});
+};
 
 var app = express();
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('views','./views');
+app.set('view engine', 'handlebars');
+app.use(logger('dev'));
 app.use(express.static(__dirname + '/public'));
 app.get('/',function(req,res){
-    res.send('hello');
+    res.render('index');
 });
+app.use(notFound);
+app.use(errorHandler);
 
 // ### Start Express
 var start = function(port){
