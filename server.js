dae394f5494e5fe0994ad4e98dfa249456b3315f
ram-manthry <ram.manthry@gmail.com>
2014-09-29 00:04:46 +1300

    added error handling

--- server.js
+++ server.js
@@ -1,62 +1,31 @@
 var express = require('express'),
-    //exphbs  = require('express-handlebars'),
-    //fs = require('fs'),
     logger = require('morgan'),
     engines = require('consolidate'),
     swig = require('swig');
 
-//// ### Middleware to handle 404
-//var notFound = function(req,res,next){
-//    res.statusCode = 404;
-//    res.description = 'Not found';
-//    res.render('404');
-//};
-//// ### Middleware to internal server errors
-//var errorHandler = function(err,req,res,next){
-//    res.render('error',{description:'Please check the URL.'});
-//};
+// ### Middleware to handle 404
+var notFound = function(req,res,next){
+    res.statusCode = 404;
+    res.description = 'Not found';
+    res.render('404',{cache: false});
+};
+// ### Middleware to internal server errors
+var errorHandler = function(err,req,res,next){
+    res.render('error',{cache: false,description:'Please check the URL.'});
+};
 
 var app = express();
 app.engine('html', engines.swig);
-//app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('views','./views');
 app.set('view engine', 'html');
 
-//
-//
-//app.set("view options", {
-//    layout: false
-//});
-//
-//app.engine('html', function(path, options, fn){
-//    var cacheLocation = path + ':html';
-//    if(typeof module.exports.cache[cacheLocation] === "string"){
-//        return fn(null, module.exports.cache[cacheLocation]);
-//    }
-//    fs.readFile(path, 'utf8', function(err, data){
-//        if(err) { return fn(err); }
-//        return fn(null, module.exports.cache[cacheLocation] = data);
-//    });
-//});
-//app.set('view engine', 'html');
-
 app.use(logger('dev'));
 app.use(express.static(__dirname + '/public'));
-//app.get('/',function(req,res){
-//    res.render('index');
-//});
 app.get('/', function(request, response,next){
-    response.render("index",{cache: false}, function (err, html) {
-        try{
-            if(err) throw err;
-            response.send(html);
-        }catch(e){
-            next(e);
-        }
-    });
+    response.render("index",{cache: false});
 });
-//app.use(notFound);
-//app.use(errorHandler);
+app.use(notFound);
+app.use(errorHandler);
 
 // ### Start Express
 var start = function(port){
